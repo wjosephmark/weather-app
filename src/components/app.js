aa401fc33a3fc058758046e07735bd0a21c9cdbd
wjosephmark <wjosephmark@gmail.com>
2020-06-25 12:40:50 -0600

    added background, search bar, and established communication with api

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,72 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React + Redux</h2>
+export default function App(){
 
-        <div style={{ color: "red" }}>
-          Warning: this is the experimental template, pardon the bugs
-        </div>
-      </div>
-    );
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const search = e => {
+    if(e.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result)
+          setQuery('')
+          console.log(result)
+        })
+    }
   }
+
+  const api = {
+    key: "f933595f4ec128352b97f105a1a4faa8",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+    
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-wrapper">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-wrapper">
+              <div className="temp">
+                {Math.round(weather.main.temp)}
+              </div>
+              <div className="weather">
+
+              </div>
+            </div>
+          </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
 }
